4be704b477989462156ca38f5a4119e1058d80c2
Margaret Scott <eudaimonious@gmail.com>
2015-03-26 20:47:47 -0400

    remove cluster initialization. connect and write to nsq

--- msgproc.rb
+++ msgproc.rb
@@ -1,15 +1,25 @@
-require 'nsq-cluster'
 require 'nsq'
 
-# Start a cluster of 3 nsqd's and 2 nsqlookupd's.
-# This will block execution until all components are fully up and running.
-cluster = NsqCluster.new(nsqd_count: 3, nsqlookupd_count: 2)
+# Seed NSQ with data
 
-# Stop the 3rd nsqd instance and wait for it to come down.
-cluster.nsqd.last.stop
+# Each message in the queue is a JSON string with a GUID video_id attribute,
+# which is used by the client to identify a video.
 
-# Start it back up again and wait for it to fully start.
-cluster.nsqd.last.start
+# There are 100,000 messages on the queue combined for 100 videos. 
+# Some videos have more plays than other videos. You can decide on the distribution 
+# of 100,000 plays across the 100 videos, but ensure there are at least 15 videos
+# with fewer than 100 plays each.
 
-# Tear down the whole cluster.
-cluster.destroy
+producer = Nsq::Producer.new(
+  nsqd: '127.0.0.1:4250',
+  topic: 'some-topic'
+)
+
+# Write a message to NSQ
+producer.write('some-message')
+
+# # Write a bunch of messages to NSQ (uses mpub)
+producer.write('one', 'two', 'three', 'four', 'five')
+
+# # Close the connection
+producer.terminate
