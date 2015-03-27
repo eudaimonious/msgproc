617f90f91f265c1ba77a4f76104d19d51049b452
Margaret Scott <eudaimonious@gmail.com>
2015-03-26 22:53:54 -0400

    increment play counts and print when small

--- consumer.rb
+++ consumer.rb
@@ -1,4 +1,5 @@
 require 'nsq'
+require 'json'
 Nsq.logger = Logger.new(STDOUT)
 consumer = Nsq::Consumer.new(
   nsqlookupd: ["127.0.0.1:4361", "127.0.0.1:4363"],
@@ -6,14 +7,22 @@ consumer = Nsq::Consumer.new(
   channel: 'some-channel'
 )
 
-puts 'hello world'
-puts consumer.size
+play_counts = Hash.new(0)
 
-# Pop a message off the queue
-msg = consumer.pop
-puts msg.body
-msg.finish
-
+100000.times do
+  # Pop a message off the queue
+  msg = consumer.pop
+  event = JSON.parse(msg.body)
+  id = event["video_id"]
+  play_counts[id] += 1
+  # For videos with fewer than 100 plays, the processor needs to the publish 
+  # the result to the client as soon as possible because with such a small sample 
+  # size, each play is of the utmost "realtime" importance.
+  if play_counts[id] < 100
+    puts "Video with id #{id} has #{play_counts[id]} views."
+  end
+  msg.finish
+end
 
 
 
